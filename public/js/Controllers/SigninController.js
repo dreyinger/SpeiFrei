fbde77e1bee9427da4c2c7d09fd72188a07d6e7d
David Reyinger <david.reyinger@dozeo.com>
2014-11-04 14:46:56 +0100

    fixed login and signup with availability check

--- public/js/Controllers/SigninController.js
+++ public/js/Controllers/SigninController.js
@@ -5,16 +5,19 @@ App.SigninController = Ember.Controller.extend({
 			if (Ember.get(this, "email") && Ember.get(this, "password")) {
 				var data = {email: Ember.get(this, "email"), password: Ember.get(this, "password")};
 				Ember.$.get( '/api/v1/user', data).done(function (d) {
-					if (d) {
+					d = JSON.parse(d);
+					if (d.auth) {
 						setCookie('auth', true, 1);
 						Ember.set(self, 'email', undefined);
 						Ember.set(self, 'password', undefined);
 						Ember.set(self, 'message', undefined);
 						self.transitionToRoute('movies');
+					} else {
+						Ember.set(self, 'message', 'Please enter a valid Username Password combination');
 					}
 				});
 			} else {
-				Ember.set(this, 'message', 'Please enter a valid Username Password combination');
+				Ember.set(self, 'message', 'Please enter a valid Username Password combination');
 			}
 		},
 		signup: function () {
